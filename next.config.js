8218e408a84e657eb436dadc8f2a872c7692eb3c
sdntsng <s@siddhant.site>
2025-08-19 13:27:50 -0700

    feat: add redirect configuration for '/meet' to external scheduling link

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/meet',
+        destination: 'https://cal.com/artimech/15min',
+        permanent: false, // set to true if you want a 308 permanent redirect
+      },
+    ]
+  },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig Dehradun
